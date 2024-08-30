075949c133f8b176b3ed8e32520757f2e57f5c21
AlejandroLondonoValle <londonovalleluisalejandro@gmail>
2024-08-30 08:58:48 -0500

    feat: Add dataanotion in class custormer

--- Models/ Customer.cs
+++ Models/ Customer.cs
@@ -1,16 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestAdmin.Models;
 
+[Table("customers")]
 public class Customer
 {
+    [Key]
     public int Id { get; set; }
+
+    [Column("name")]
+    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
     public string Name { get; set; }
+
+    [Column("adress")]
+    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
     public string Address { get; set; }
+
+    [Column("phone_number")]
+    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
     public string PhoneNumber { get; set; }
+    
+    [Column("email")]
+    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
     public string Email { get; set; }
 
 }
