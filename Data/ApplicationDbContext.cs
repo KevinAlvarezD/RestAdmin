b3bf950a9d61fe862a21251bed0e7031b6304c15
CODEALEJO <salomee579@gmail.comgit config --global user.email salomee579@gmail.comgit config --global user.email salomee579@gmail.com>
2024-08-30 12:01:40 -0500

    feat: order controller creation

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,14 +5,17 @@ using RestAdmin.Models;
 namespace LibrarySystem.Data;
 public class ApplicationDbContext : DbContext
 {
-   public DbSet<Product> Products { get; set; }
-   
-   public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
 
-   public DbSet<Table> Tables { get; set; }
+    public DbSet<Customer> Customers { get; set; }
 
-   public DbSet<Employee> Employees { get; set; }
-   
+    public DbSet<Table> Tables { get; set; }
+
+    public DbSet<Employee> Employees { get; set; }
+
+
+
+    public DbSet<Ordered> Ordereds { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
