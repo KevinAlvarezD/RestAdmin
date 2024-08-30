6067a7ec0d0d0b8539c123d2202c25143fc1d292
AlejandroLondonoValle <londonovalleluisalejandro@gmail>
2024-08-30 06:41:55 -0500

    feat: Add table Payment

---
+++ Models/Payment.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAdmin.Models;
+
+public class Payment
+{
+    public int IdPayment { get; set; }
+    public int IdInvoice { get; set; }
+    public DateTime DatePayment { get; set; }
+    public double Amount { get; set; }
+    public string PaymentMethod { get; set; }
+}
