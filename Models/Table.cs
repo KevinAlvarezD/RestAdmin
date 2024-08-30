a440a93ce7d1b03c8476d22d30618887a2c34628
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-08-30 09:15:29 -0500

    feat: The dataNotation was added to Table

--- Models/Table.cs
+++ Models/Table.cs
@@ -1,14 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace RestAdmin.Models;
+[Table("tables")]
 
 public class Table
 {
+    [Key]
+    [Column("id")]
     public int Id { get; set; }
-    public string TableNumber { get; set; }
+    [Column("table_number")]
+    [MaxLength(255, ErrorMessage = "The Name field must be at most {1} characters.")]
+    public string? TableNumber { get; set; }
+    [Column("capacity")]
+    [MaxLength(11, ErrorMessage = "The Name field must be at most {1} characters.")]
     public int Capacity { get; set; }
 
 }
