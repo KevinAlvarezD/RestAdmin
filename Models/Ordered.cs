051b7727731f310c8cece691f71c7d7c4ceb0d79
AlejandroLondonoValle <londonovalleluisalejandro@gmail>
2024-08-30 09:09:29 -0500

    feat: Add dataanotation in class ordereds

--- Models/Ordered.cs
+++ Models/Ordered.cs
@@ -1,16 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestAdmin.Models;
 
+[Table("ordereds")]
 public class Ordered
 {
+    [Key]
+    [Column("id")]
+    [MaxLength(11, ErrorMessage = "The field must be at most {1} character.")]
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public int IdCostumer { get; set; }
-    public int IdTable { get; set; }
-    public int Employee { get; set; }
 
+    [Column("name")]
+    [MaxLength(50, ErrorMessage = "The field must be at most {1} character.")]
+    public string? Name { get; set; }
+
+    [Column("id_customer")]
+    [MaxLength(11, ErrorMessage = "The field must be at most {1} character.")]
+    public int IdCustomer { get; set; }
+
+    [Column("id_table")]
+    [MaxLength(11, ErrorMessage = "The field must be at most {1} character.")]
+    public int IdTable { get; set; }
+
+    [Column("employee")]
+    [MaxLength(11, ErrorMessage = "The field must be at most {1} character.")]
+    public int Employee { get; set; }
 }
