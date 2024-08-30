30bd3fb9180177b6211aee9e1631968cfc0f2dc4
KevinAlvarezD <kevinadozanam23@gmail.com>
2024-08-30 08:50:39 -0500

    feat: dataNotation was added to Administrator

--- Models/Administrator.cs
+++ Models/Administrator.cs
@@ -1,15 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RestAdmin.Models;
 
+[Table("administrators")]
 public class Administrator
 {
+    [Key]
+    [Column("id_administrator")]
     public int Id { get; set; }
+    [Column("name")]
+    [MaxLength(50, ErrorMessage = "The Name field must be at most {1} characters.")]
     public string Name { get; set; }
+    [Column("username")]
+    [MaxLength(50, ErrorMessage = "The Username field must be at most {1} characters.")]
     public string UserName { get; set; }
+    [Column("password")]
+    [MaxLength(255, ErrorMessage = "The Password field must be at most {1} characters.")]
     public string Password { get; set; }
 
 }
